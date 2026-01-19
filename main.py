2af601d81ae1a00d1a99de0bc1486930bb5dc4b2
yesh0092 <yesh00941@gmail.com>
2026-01-19 18:36:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -387,8 +387,6 @@ async def announce(ctx, *, message: str):
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(
-        status=discord.Status.idle,
-        activity=discord.Game("!help | Open ticket")
-    )
-    print(f"✅ {bot.user} is online (idle)")
+    print(f"✅ {bot.user} is online")
+
+bot.run(TOKEN)
